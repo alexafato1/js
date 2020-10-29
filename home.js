689f56a106f61c8f6e759175950f9aa87f7ea788
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-10-29 13:40:59 +0300

    4.4

--- home.js
+++ home.js
@@ -95,6 +95,62 @@ function rpsFrontEnd (humanImageChoice, botImageChoice, finalMessage) {
       document.getElementById('flex-box-rps-div').appendChild(humanDiv)
       document.getElementById('flex-box-rps-div').appendChild(messageDiv)
       document.getElementById('flex-box-rps-div').appendChild(botDiv)
-     
+      
+
+}
+ function reload (){
+    location.reload();
+ }
+
+ //4
+ var all_buttons = document.getElementsByTagName('button');
+ var copyAllButtons = [];
+ for (let i=0; i < all_buttons.length; i++){
+     copyAllButtons.push(all_buttons[i].classList[1])
+ }
+
+ console.log(copyAllButtons)
+
+ function buttonColorChange(buttonThingy){
+     if(buttonThingy.value === 'red'){
+         buttonsRed();
+         } else if (buttonThingy.value === 'green'){
+             buttonsGreen();
+         } else if (buttonThingy.value === 'reset'){
+            buttonsReset();
+        } else {
+            randomColors();
+        }
+ }
+
+ function buttonsRed() {
+     for (let i=0; i < all_buttons.length; i++){
+         all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+         all_buttons[i].classList.add('btn-danger');
+     }
+ }
+
+ function buttonsGreen() {
+    for (let i=0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonsReset() {
+    for (let i=0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors() {
+    var choice = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
+    for (let i=0; i < all_buttons.length; i++){
+        var randomNumber = Math.floor(Math.random() * 4)
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add(choice[randomNumber])
+    }
+    
 
 }
